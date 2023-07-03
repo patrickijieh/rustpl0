2b808e91c8c5c05b2d9d81fd258a1482ce7e47d9
patrickijieh <reapful9234@gmail.com>
2023-07-02 20:56:02 -0400

    Changed letter casing for reserved_types.rs constants

--- src/reserved_types.rs
+++ src/reserved_types.rs
@@ -1,13 +1,13 @@
 use crate::token::TokenType::{self, *};
 
-const reserved_words: [&str; 15] = [
+const RESERVED_WORDS: [&str; 15] = [
   "const", "var", "procedure",
   "call", "begin", "end",
 	"if", "then", "else", "while", "do",
   "read", "write", "skip", "odd",
 ];
 
-const reserved_types: [TokenType; 15] = [
+const RESERVED_TYPES: [TokenType; 15] = [
   Constsym, Varsym, Procsym,
   Callsym, Beginsym, Endsym,
   Ifsym, Thensym, Elsesym, Whilesym, Dosym,
@@ -15,9 +15,9 @@ const reserved_types: [TokenType; 15] = [
 ];
 
 pub fn get_reserved_word(word: &str) -> TokenType {
-  for i in 0..reserved_words.len() {
-    if word == reserved_words[i] {
-      return reserved_types[i];
+  for i in 0..RESERVED_WORDS.len() {
+    if word == RESERVED_WORDS[i] {
+      return RESERVED_TYPES[i];
     }
   }
   Identsym
